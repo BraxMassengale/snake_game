286d6e9f1b7fe70a275d00b4e3b83df01bb4047b
Braxton <Braxtonmassengale@outlook.com>
2020-05-28 16:51:58 +0200

    initial commit

---
+++ main.rb
@@ -0,0 +1,159 @@
+require 'ruby2d'
+
+set background: "navy"
+set fps_cap: 20
+
+#width is 640px by default / 20 = 32
+#height is 480px by default / 20 = 24
+GRID_SIZE = 20
+GRID_WIDTH = Window.width / GRID_SIZE
+GRID_HEIGHT = Window.height / GRID_SIZE
+
+class Snake
+  attr_writer :direction
+
+  def initialize
+    @positions = [[2, 0], [2, 1], [2, 2], [2, 3]]
+    @direction = "down"
+    @growing = false
+  end # from initialize
+
+  def draw
+    @positions.each do |position|
+      Square.new(x: position[0] * GRID_SIZE, y: position[1] * GRID_SIZE, size: GRID_SIZE - 1, color: "white")
+      end # draw.loop
+  end # from draw
+
+  def move
+    if !@growing
+      @positions.shift
+    end
+
+    case @direction
+    when "down"
+      @positions.push(new_coordinates(head[0], head[1] + 1))
+    when "up"
+      @positions.push(new_coordinates(head[0], head[1] - 1))
+    when "left"
+      @positions.push(new_coordinates(head[0] - 1, head[1]))
+    when "right"
+      @positions.push(new_coordinates(head[0] + 1, head[1]))
+    end # from move.case
+    @growing = false
+  end #from move
+
+  def can_change_direction?(new_direction)
+    case @direction
+    when 'up' then new_direction != 'down'
+    when 'down' then new_direction != 'up'
+    when 'right' then new_direction != 'left'
+    when 'left' then new_direction != 'right'
+    end # from can_change_direction.case
+  end # from can_change_direction
+
+  def new_coordinates(x, y)
+    [x % GRID_WIDTH, y % GRID_HEIGHT]
+  end
+
+  def x
+    head[0]
+  end
+
+  def y
+    head[1]
+  end
+
+  def grow
+    @growing = true
+  end
+
+  def hit_self?
+    @positions.uniq.length != @positions.length
+  end
+
+  private
+
+  def head
+    @positions.last
+  end # from head
+end # from Snake
+
+class Game
+  def initialize
+    @score = 0
+    @ball_x = rand(GRID_WIDTH)
+    @ball_y = rand(GRID_HEIGHT)
+    @finished = false
+  end # from initialize
+
+  def draw
+    unless finished?
+      Square.new(x: @ball_x * GRID_SIZE, y: @ball_y * GRID_SIZE, size: GRID_SIZE, color: 'yellow')
+    end
+      Text.new(text_message, color: 'green', x: 10, y: 10, size: 25)
+  end # from draw
+
+  def snake_hit_ball?(x, y)
+    @ball_x === x && @ball_y === y
+  end
+
+  def record_hit
+    @score += 1
+    @ball_x = rand(GRID_WIDTH)
+    @ball_y = rand(GRID_HEIGHT)
+  end
+
+  def finish
+    @finished = true
+  end
+
+  def finished?
+    @finished
+  end
+
+  private
+
+  def text_message
+    if finished?
+      "Game over, your score was #{@score}. Press r to restart."
+    else
+      "Score: #{@score}"
+    end
+  end
+
+end # from Game
+
+snake = Snake.new
+game = Game.new
+
+update do
+  clear
+  unless game.finished?
+    snake.move
+  end
+
+  snake.draw
+  game.draw
+
+  if game.snake_hit_ball?(snake.x, snake.y)
+    game.record_hit
+    snake.grow
+  end
+
+  if snake.hit_self?
+    game.finish
+  end
+end # from update
+
+on(:key_down) { |event|
+  if %w[up down left right].include?(event.key)
+    if snake.can_change_direction?(event.key)
+      snake.direction = event.key
+    end # from direction change
+  elsif event.key == 'r'
+    snake = Snake.new
+    game = Game.new
+  end # from keyboard_event.if
+}
+
+show
